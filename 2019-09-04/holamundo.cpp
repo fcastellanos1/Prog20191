63788d0e979539a3cecc2b9e81834d89bb869729
Fabio Guillermo Castellanos Lenes <fcastellanos@darkstar.org>
2019-09-06 07:52:43 -0500

    sfdfgdgegegdsdfsdfdsfsdfsdt q
    q
    wq

---
+++ 2019-09-04/holamundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main (void)
+{
+  double x = 0.0;
+  std:: cout << "Escribe el numero :" << std::endl;
+  std:: cin >> x;
+  std:: cout << "He leido :" << x << std::endl;
+// comentario
+  
+  return 0;
+}
